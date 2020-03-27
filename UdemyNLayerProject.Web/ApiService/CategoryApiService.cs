a3357e43ec227b4745aab8a5973917db48757f68
alieryaman <postauyelik@gmail.com>
2020-03-27 12:58:15 +0300

    CategoryApiService kodlanması yapıldı

---
+++ UdemyNLayerProject.Web/ApiService/CategoryApiService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace UdemyNLayerProject.Web.ApiService
+{
+    public class CategoryApiService
+    {
+        private readonly HttpClient _httpClient;
+
+    }
+}
