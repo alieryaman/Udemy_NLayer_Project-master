922dfceb89ce9f22896c770229c0a1e306191c05
alieryaman <postauyelik@gmail.com>
2020-03-27 11:32:33 +0300

    Add project files.

---
+++ UdemyNLayerProject.Data/DesignTimeIdentityDbContextFactory.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UdemyNLayerProject.Data
+{
+    class DesignTimeIdentityDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+
+
+
+        public AppDbContext CreateDbContext(string[] args)
+        {
+
+
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-NV0GSA0\\SQLEXPRESS;Initial Catalog=UdemyNLayerDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+            return new AppDbContext(optionsBuilder.Options);
+
+
+
+
+
+        }
+
+
+
+
+    }
+}
